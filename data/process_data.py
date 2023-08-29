61141c78c7c97c89421fabb775f1f833098a5e2c
MOMARIA <141179961+MOMARIA@users.noreply.github.com>
2023-08-29 12:35:03 +0200

    process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -5,13 +5,15 @@ from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
     """
-    Load dataframe from filepaths - messages and categories
-    Input: 
-    file path of messages data
-    file path of categories data
-    output: df - pandas DataFrame, merged dataset from messages and categories
-    """
+    Loads a DataFrame by merging message and category data from specified file paths.
     
+    Parameters:
+    - messages_file_path: String representing the file path of the messages data.
+    - categories_file_path: String representing the file path of the categories data.
+    
+    Returns:
+    - DataFrame: A Pandas DataFrame containing the merged dataset from the provided messages and categories file paths.
+    """
     # message dataset
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
@@ -21,11 +23,15 @@ def load_data(messages_filepath, categories_filepath):
     
 
 def clean_data(df):
-    '''
-    Clean the input pandas DataFrame
-    Input: df - pandas DataFrame.
-    Output：df - cleaned pandas DataFrame.
-    '''
+    """
+    Cleans the input Pandas DataFrame.
+    
+    Parameters:
+    - df: A Pandas DataFrame that needs to be cleaned.
+    
+    Returns:
+    - DataFrame: A cleaned Pandas DataFrame.
+    """
     categories = df["categories"].str.split(";", expand=True)
     # Select the first row of the categories dataframe
     row = categories.iloc[0,:]
@@ -46,11 +52,14 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     """
-    Save the cleaned data.
-    Input:
-    df-pandas dataframe containing cleaned data of messages and categories.
-    filename for the output database.
-    Output:None.
+    Saves the cleaned data to a specified database file.
+    
+    Parameters:
+    - df: A Pandas DataFrame containing the cleaned data for messages and categories.
+    - filename: String representing the name of the output database file.
+    
+    Returns:
+    - None
     """
     engine = create_engine('sqlite:///'+ database_filename)
     df.to_sql('message', engine, index = False)
@@ -74,13 +83,7 @@ def main():
         print('Cleaned data saved to database!')
     
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
-
-
+        print("Provide the file paths for the messages and categories datasets as the first and second arguments, respectively. Also, specify the database file path where you'd like to save the cleaned data as the third argument.\n\nUsage Example: python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db")
+        
 if __name__ == '__main__':
     main()
